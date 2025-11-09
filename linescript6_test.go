9ff827328d1a776027d47e7a4ca5cd64754484ba
Drew LeSueur <drewalex@gmail.com>
2025-11-09 14:52:13 +0000

    wip

--- linescript6_test.go
+++ linescript6_test.go
@@ -14,6 +14,28 @@ func ExampleE_hello() {
 	// hello
 }
 
+func ExampleE_helloUpper() {
+	E(`
+        say1 upper .hello
+        .Cloud upper
+        say1
+        
+        say1 .wallpaper upper
+        
+        .grape upper, say1
+        
+        .square upper; say1
+        
+    `)
+
+	// Output:
+	// HELLO
+	// CLOUD
+	// WALLPAPER
+	// GRAPE
+	// SQUARE
+}
+
 func ExampleParseString_yo() {
 	tokens := ParseString(`
         say1 .hello
